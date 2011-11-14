a96ee49f3568d1587d8e4c3b65ccf8c7c19c0516
David Fonvielle <davidfonvielle@Davids-MacBook-Air-2.local>
2011-11-14 15:05:19 -0600

    Creation of aha_list app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AhaList::Application.config.secret_token = '2131cf0a60cd68d47938936c1592d2c07eca873a725c81eb54ee93b1405ab527af8f92e53e24bc1b1820b17ed0c06e9fb38a239611a5482f3673619881f0e63d'
